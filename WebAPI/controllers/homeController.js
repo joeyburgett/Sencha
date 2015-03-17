a2806af249b54a135ff94486713e10d5e27670fc
joey burgett <joey.burgett@hotmail.com>
2015-03-16 19:18:37 -0500

    DRY!

--- WebAPI/controllers/homeController.js
+++ WebAPI/controllers/homeController.js
@@ -3,68 +3,52 @@
     
     var data = require('../data');
 
+    function actionResponse(error, records, response){
+        if (error) {
+            response.status(404).send({success: false, msg: error});
+        } else {
+            response.set('Content-Type', 'application/json');
+            response.status(200).send({success: true, data: records});
+        }
+    };
+
     homeController.init = function (app) {
         
         app.get("/api", function (request, response) {
-            data.getCompanies(function(error, records) {
-                if (error) {
-                    response.status(404).send(error);
-                } else {
-                    response.set('Content-Type', 'application/json');
-                    response.status(200).send({ data: records });
-                }
+            data.getCompanies(function(error, records){
+                actionResponse(error, records, response);
             });
         });
 
         app.get('/api/:id', function (request, response) {
             var id = request.params.id;
-            data.getCompany(id, function(error, record) {
-                if (error) {
-                    response.status(404).send(error);
-                } else {
-                    response.set('Content-Type', 'application/json');
-                    response.status(200).send(record);
-                }
+            data.getCompany(id, function(error, records) {
+                actionResponse(error, records, response);
             });
         });
 
         app.put('/api/:id', function(request, response) {
             var body = request.body;
             var id = request.params.id;
-            data.createCompany(body, function (error, record) {
-                if (error) {
-                    response.status(404).send({ success: false, message: error });
-                } else {
-                    response.set('Content-Type', 'application/json');
-                    response.status(200).send({ success: true, data: record });
-                }
+            data.createCompany(body, function (error, records) {
+                actionResponse(error, records, response);
             });
         });
 
         app.post('/api', function (request, response) {
             var body = request.body;
-
-            data.createCompany(body, function (error, record) {
-                if (error) {
-                    response.status(404).send({ success: false, message: error });
-                } else {
-                    response.set('Content-Type', 'application/json');
-                    response.status(200).send({ success: true, data: record });
-                }
+            data.createCompany(body, function (error, records) {
+                actionResponse(error, records, response);
             });
         });
 
         app.delete('/api/:id', function (request, response) {
             var id = request.params.id;
             data.deleteCompany(id, function (error, success) {
-                if (error) {
-                    response.status(404).send(error);
-                } else {
-                    response.set('Content-Type', 'application/json');
-                    response.status(200).send({ success: success });
-                }
+                actionResponse(error, [], response);
             });
         });
+
     };
 
 })(module.exports);
